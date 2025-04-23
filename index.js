2ca38addf0b457c6d7db70647deded157ec5582f
yassine snaiki <yassnaiki@gmail.com>
2025-04-23 09:59:54 +0100

    updating index file

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-const express = require('express');
-const cors = require('cors');
+import CategoryRoutes from './src/routes/CategoryRoutes.js';
+import BookRoutes from './src/routes/BookRoutes.js';
+import UserRoutes from './src/routes/UserRoutes.js';
+import LoanRoutes from './src/routes/LoanRoutes.js';
+import dotenv from 'dotenv';
+import authRoutes from './src/routes/AuthRoutes.js';
+import { authenticateToken } from './src/middleware/authenticateToken.js';
+
+
+
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const PORT = 4000;
+dotenv.config();
+
 
 // Middleware to parse JSON
 app.use(express.json());
 app.use(cors());
+
+app.use('/api/auth', authRoutes);
+
+app.get('/api/protected', authenticateToken, (req, res) => {
+    res.json({ message: 'You are authorized!', user: req.user });
+  });
+  
 // A test route
 app.get('/', (req, res) => {
   res.send('Hello from Express!');
 });
+app.use('/api/books', BookRoutes);
+app.use('/api/categories', CategoryRoutes);
+app.use('/api/users', UserRoutes);
+app.use('/api/loans', LoanRoutes);
 
-// Start the server
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
